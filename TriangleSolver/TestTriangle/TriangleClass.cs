b99b037c80b1b67c15317295c3ff44cf1278e1cb
djbosmiya <djbosmiya@gmail.com>
2023-06-08 04:26:22 -0400

    Test Case for Isosceles Triangle

--- TriangleSolver/TestTriangle/TriangleClass.cs
+++ TriangleSolver/TestTriangle/TriangleClass.cs
@@ -21,10 +21,65 @@ namespace TestTriangle
             int thirdSide = 50;
 
             string expected = "The triangle is valid and is an EQUILATERAL";
+
             //Act
             string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
 
             //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        //Test Case-2 for Isosceles Triangle
+        [Test]
+        public void Isosceles_input50and20and50()
+        {
+            // Arrange
+            int firstSide = 50;
+            int secondSide = 20;
+            int thirdSide = 50;
+
+            string expected = "The triangle is valid and is an ISOSCELES";
+
+            // Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        //Test Case-3 for Isosceles Triangle
+        [Test]
+        public void Isosceles_input20and50and50()
+        {
+            // Arrange
+            int firstSide = 20;
+            int secondSide = 50;
+            int thirdSide = 50;
+
+            string expected = "The triangle is valid and is an ISOSCELES";
+
+            // Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        //Test Case-4 for Isosceles Triangle
+        [Test]
+        public void Isosceles_input50and50and20()
+        {
+            // Arrange
+            int firstSide = 50;
+            int secondSide = 50;
+            int thirdSide = 20;
+
+            string expected = "The triangle is valid and is an ISOSCELES";
+
+            // Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
             Assert.AreEqual(expected, actual);
         }
     }
