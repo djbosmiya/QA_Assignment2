5a8d54829a0ccab5b6c36dce8e371ed1fc030131
djbosmiya <djbosmiya@gmail.com>
2023-06-08 03:53:28 -0400

    Test Case for Equilateral Triangle

--- TriangleSolver/TestTriangle/TriangleClass.cs
+++ TriangleSolver/TestTriangle/TriangleClass.cs
@@ -11,6 +11,21 @@ namespace TestTriangle
 {
     public class TriangleClass
     {
-       
+        //Test Case-1 for Equilateral Triangle
+        [Test]
+        public void Equilateral_input50and50and50()
+        {
+            //Arrange
+            int firstSide = 50;
+            int secondSide = 50;
+            int thirdSide = 50;
+
+            string expected = "The triangle is valid and is an EQUILATERAL";
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
